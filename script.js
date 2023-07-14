f2b6cbfbb4efa387db364c5e27580f22c8b8c2dc
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-13 20:08:57 -0500

    feat: add images and styles

---
+++ script.js
@@ -0,0 +1,6 @@
+function ToggleMenu(){
+  const menuToggle = document.querySelector('.menuToggle');
+  const navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
